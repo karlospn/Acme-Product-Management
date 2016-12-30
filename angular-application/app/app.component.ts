261607ff054865d1b1e69009328159dd9b0042d1
Carlos Pons <carlosponsn@outlook.com>
2016-12-30 16:36:01 +0100

    Messing around with app.component

--- angular-application/app/app.component.ts
+++ angular-application/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-@Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+@
+Component({
+    selector: "pm-app",
+    template: "<h1>{{pageTitle}}</h1>"
 })
-export class AppComponent { }
+
+export class AppComponent {
+    private pageTitle : string = "Acme Product Management";
+    
+}
